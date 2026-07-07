2bfabd68bf6a1aae7030cb5af28bc03f6b3e71fa
VAN MINH <nguyminh16@gmail.com>
2026-07-07 19:59:21 +0700

    bai tho banh troi nuoc

---
+++ buoi1-bai1/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("than em thoi trang phan em tron ,");
+        System.out.println(" bay noi ba chim voi nuoc non .") ;
+        System.out.println(" ran nat mac dau tay ke nan ,");
+        System.out.println(" nhung em van giu tam long son .") ;
+        }
+    }
